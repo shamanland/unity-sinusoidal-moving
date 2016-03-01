f46e24326548e7cef2f98baead846b6b8acc3dd5
Oleksii Kropachov <o.kropachov@shamanland.com>
2016-03-01 12:55:22 +0200

    Initial commit

---
+++ Assets/Mover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public Rigidbody rb;
+
+	public Transform target;
+	public float speed;
+	public float amplitude;
+	public float frequency;
+
+	private float startTime;
+	private Vector3 direction;
+	private Vector3 orthogonal;
+
+	void Start() {
+		startTime = Time.time;
+		direction = (target.position - transform.position).normalized;
+		orthogonal = new Vector3 (-direction.z, 0, direction.x);
+	}
+
+	void FixedUpdate () {
+		float t = Time.time - startTime;
+		rb.velocity = direction * speed + orthogonal * amplitude * Mathf.Sin (frequency * t);
+	}
+}
